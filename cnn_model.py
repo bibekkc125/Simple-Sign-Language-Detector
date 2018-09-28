5fa253f4c54560922fec601d223d0c8a364a7933
Bibek KC <35173479+bibekkc125@users.noreply.github.com>
2018-09-28 08:30:47 +0545

    Update cnn_model.py

--- cnn_model.py
+++ cnn_model.py
@@ -1,23 +1,27 @@
-# Part 1 - Building the CNN
-#importing the Keras libraries and packages
 from keras.models import Sequential
 from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense, Dropout
 from keras import optimizers
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix,classification_report
+import numpy as np
+
 
 # Initialing the CNN
 classifier = Sequential()
 
-# Step 1 - Convolutio Layer 
-classifier.add(Convolution2D(32, 3,  3, input_shape = (200, 200, 3), activation = 'relu'))
-
-#step 2 - Pooling
+# Step 1 - Convolution Layer
+classifier.add(Convolution2D(32, (3,3), input_shape = (64, 64, 3), activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size =(2,2)))
 
 # Adding second convolution layer
-classifier.add(Convolution2D(32, 3,  3, activation = 'relu'))
+classifier.add(Convolution2D(32,(3,3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size =(2,2)))
+
+#Adding 3rd convolution layer
+classifier.add(Convolution2D(32,(3,3), activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size =(2,2)))
 
 #Step 3 - Flattening
@@ -26,13 +30,16 @@ classifier.add(Flatten())
 #Step 4 - Full Connection
 classifier.add(Dense(256, activation = 'relu'))
 classifier.add(Dropout(0.5))
-classifier.add(Dense(5, activation = 'softmax'))
-
+classifier.add(Dense(26, activation = 'softmax'))
+classifier.summary()
+sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 #Compiling The CNN
 classifier.compile(
-              optimizer = optimizers.SGD(lr = 0.01),
+              optimizer = sgd,
               loss = 'categorical_crossentropy',
               metrics = ['accuracy'])
+#Model summmary
+classifier.summary()
 
 #Part 2 Fittting the CNN to the image
 from keras.preprocessing.image import ImageDataGenerator
@@ -46,31 +53,53 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 
 training_set = train_datagen.flow_from_directory(
         'mydata/training_set',
-        target_size=(200, 200),
+        target_size=(64, 64),
         batch_size=32,
         class_mode='categorical')
 
 test_set = test_datagen.flow_from_directory(
         'mydata/test_set',
-        target_size=(200, 200),
+        target_size=(64, 64),
         batch_size=32,
         class_mode='categorical')
 
-classifier.fit_generator(
+mod = classifier.fit_generator(
         training_set,
-        steps_per_epoch=80,
-        epochs=25,
+        steps_per_epoch=800,
+        epochs=6,
         validation_data = test_set,
-        validation_steps = 40
+        validation_steps = 6500
       )
 
+Y_pred = classifier.predict_generator(test_set, 6500 // 32+1)
+y_pred = np.argmax(Y_pred, axis=1)
+print('Confusion Matrix')
+print(confusion_matrix(test_set.classes, y_pred))
+print('Classification Report')
+target_names = ['A', 'B', 'C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+print(classification_report(test_set.classes, y_pred, target_names=target_names))
+
+#confusion_matrix(,test_set)
+print(mod.history.keys())
+# summarize history for accuracy
+plt.plot(mod.history['acc'])
+plt.plot(mod.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(mod.history['loss'])
+plt.plot(mod.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
 #Saving the model
-import h5py
-classifier.save('Trained_model.h5')
-
-
-
-
+classifier.save('Trained_model_5.h5')
 
 
 
